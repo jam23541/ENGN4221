43b517a958b01ebe3d67087f587ac66b0e9edb22
Yunan Chen <u5137524@anu.edu.au>
2017-03-27 01:16:40 +1100

    creating unity project

---
+++ TBG/Assets/Scripts/General/fake_data_arena.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fake_data_arena : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
